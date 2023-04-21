8e37236bf61f0732b819edb7ec4376abfc4f72d8
VickerTan <vickerpangestan@gmail.com>
2023-04-21 22:42:13 +0800

    Create calculation logic for hour and minutes

--- src/CalcButton.jsx
+++ src/CalcButton.jsx
@@ -1,5 +1,23 @@
 const CalcButton = () => {
-    function handleClick(e) {}
+    function handleClick(e) {
+        e.preventDefault();
+        const outTime = document.getElementById("out-time").value;
+        const inTime = document.getElementById("in-time").value;
+
+        const hour = inTime.slice(0, 2) - outTime.slice(0, 2);
+        let hourUsed = hour < 0 ? hour + 24 : hour;
+
+        const mins = inTime.slice(-2) - outTime.slice(-2);
+        let minsUsed = 0;
+        if (mins < 0) {
+            minsUsed = mins + 60;
+            hourUsed--;
+        } else {
+            minsUsed = mins;
+        }
+
+        console.log(hourUsed, minsUsed);
+    }
 
     return (
         <button className="calc-button" onClick={handleClick}>
