3c49e9028cf669d09eca9d54f24072ed02667312
VickerTan <vickerpangestan@gmail.com>
2023-04-21 16:17:25 +0800

    Insert current time into input box on button click

--- src/ClockButton.jsx
+++ src/ClockButton.jsx
@@ -1,9 +1,16 @@
 const ClockButton = (props) => {
+    function handleClick(e) {
+        e.preventDefault();
+        const timeInput = e.target.parentNode.querySelector("input");
+
+        const hour = new Date().getHours();
+        const mins = new Date().getMinutes();
+        const curTime = hour + ":" + String(mins).padStart(2, "0");
+        timeInput.value = curTime;
+    }
+
     return (
-        <button
-            className="clock-button"
-            onClick={() => console.log(`${props.icon} button clicked`)}
-        >
+        <button className="clock-button" onClick={handleClick}>
             {props.icon}
         </button>
     );
