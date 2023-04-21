f57e225607cb9fa151444f723e84ff8fe2f29036
VickerTan <vickerpangestan@gmail.com>
2023-04-21 20:41:03 +0800

    Remove unnecessary time form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { createRoot } from "react-dom/client";
 import Indicator from "./Indicator";
 import TimeForm from "./TimeForm";
-import Note from "./Note";
+import QnA from "./Note";
 
 const App = () => {
     return (
         <div className="content-box">
             <Indicator />
             <TimeForm sequence="1" />
-            <TimeForm sequence="2" />
-            <Note meals="lunch" />
-            <Note meals="dinner" />
+            <QnA text="What did you have for lunch?" />
+            <QnA text="What did you have for dinner?" />
         </div>
     );
 };
