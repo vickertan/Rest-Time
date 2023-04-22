453db60bd393abe36ebbe9c7373523a372904f75
VickerTan <vickerpangestan@gmail.com>
2023-04-22 22:21:20 +0800

    Export getCurTime function

--- src/ClockButton.jsx
+++ src/ClockButton.jsx
@@ -1,16 +1,8 @@
+import getCurTime from "./tasks/getCurTime";
+
 const ClockButton = (props) => {
-    function handleClick(e) {
-        e.preventDefault();
-        const timeInput = e.target.parentNode.querySelector("input");
-
-        const hour = new Date().getHours();
-        const mins = new Date().getMinutes();
-        const curTime = hour + ":" + String(mins).padStart(2, "0");
-        timeInput.value = curTime;
-    }
-
     return (
-        <button className="clock-button" onClick={handleClick}>
+        <button className="clock-button" onClick={getCurTime}>
             {props.icon}
         </button>
     );
