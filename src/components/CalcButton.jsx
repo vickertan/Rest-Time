dfc7616f6022d0e7fbaa7d8c26ba9a782000b4af
VickerTan <vickerpangestan@gmail.com>
2023-04-24 11:29:04 +0800

    Define variable

--- src/components/CalcButton.jsx
+++ src/components/CalcButton.jsx
@@ -2,9 +2,12 @@ import calculateTime from "../tasks/calculateTime";
 
 const CalcButton = (props) => {
     function handleClick(e) {
+        const outTime = document.getElementById("out-time").value;
+        const inTime = document.getElementById("in-time").value;
+
         e.preventDefault();
 
-        const time = calculateTime();
+        const time = calculateTime(outTime, inTime);
 
         props.setHourLeft(props.hourLeft - time.slice(0, 2));
         props.setMinsLeft(props.minsLeft - time.slice(-2));
