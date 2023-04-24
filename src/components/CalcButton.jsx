6dc8a95d538c0fcd5c8c277ad46f1ae95295aa63
VickerTan <vickerpangestan@gmail.com>
2023-04-24 11:58:36 +0800

    Prevent calculating on same input value

--- src/components/CalcButton.jsx
+++ src/components/CalcButton.jsx
@@ -2,18 +2,24 @@ import calculateTime from "../tasks/calculateTime";
 
 const CalcButton = (props) => {
     function handleClick(e) {
-        const outTime = document.getElementById("out-time").value;
-        const inTime = document.getElementById("in-time").value;
+        let outTime = document.getElementById("out-time").value;
+        let inTime = document.getElementById("in-time").value;
         let time = "";
 
         e.preventDefault();
 
-        if (outTime !== "" && inTime !== "") {
-            time = calculateTime(outTime, inTime);
+        if (outTime !== inTime) {
+            if (outTime && inTime) {
+                time = calculateTime(outTime, inTime);
+                props.setHourLeft(props.hourLeft - time.slice(0, 2));
+                props.setMinsLeft(props.minsLeft - time.slice(-2));
+                document.getElementById("out-time").value = "";
+                document.getElementById("in-time").value = "";
+            }
+        } else {
+            // change submit button color
+            console.log("Button color change");
         }
-
-        props.setHourLeft(props.hourLeft - time.slice(0, 2));
-        props.setMinsLeft(props.minsLeft - time.slice(-2));
     }
 
     return (
