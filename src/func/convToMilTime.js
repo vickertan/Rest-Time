48474ca43ede7de12fe49380c9a8cbd9af09d174
VickerTan <vickerpangestan@gmail.com>
2023-05-08 12:06:03 +0800

    Fix hour value when time is exceeded

--- src/func/convToMilTime.js
+++ src/func/convToMilTime.js
@@ -1,5 +1,11 @@
 export default function convToMilTime(mins, overLimit) {
     const min = mins % 60;
+
+    if (mins < 0) {
+        const hour = Math.ceil(mins / 60);
+    } else {
+        const hour = Math.floor(mins / 60);
+    }
     const hour = Math.floor(mins / 60);
 
     let milTime =
