02187ed4cf7a0772b859879e78e131b42265a7df
VickerTan <vickerpangestan@gmail.com>
2023-05-08 15:17:20 +0800

    Remove excessive const variable

--- src/func/convToMilTime.js
+++ src/func/convToMilTime.js
@@ -1,12 +1,12 @@
 export default function convToMilTime(mins, overLimit) {
     const min = mins % 60;
+    let hour = 0;
 
     if (mins < 0) {
-        const hour = Math.ceil(mins / 60);
+        hour = Math.ceil(mins / 60);
     } else {
-        const hour = Math.floor(mins / 60);
+        hour = Math.floor(mins / 60);
     }
-    const hour = Math.floor(mins / 60);
 
     let milTime =
         String(hour).padStart(2, "0") + ":" + String(min).padStart(2, "0");
