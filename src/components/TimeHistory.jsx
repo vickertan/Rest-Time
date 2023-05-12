341c9913d4de95f46fcf8ef355c1637a5fc90f46
VickerTan <vickerpangestan@gmail.com>
2023-05-12 17:11:37 +0800

    Display total time used for user

--- src/components/TimeHistory.jsx
+++ src/components/TimeHistory.jsx
@@ -1,14 +1,21 @@
-export default function TimeHistory({ timeList }) {
+import convToMilTime from "../func/convToMilTime";
+
+export default function TimeHistory({ timeList, totalMinsUsed }) {
     return (
         <div className="time-history">
             {!timeList.length ? (
                 <p className="default">No record yet</p>
             ) : (
-                <div className="inner-history-box">
-                    {timeList.map((time, index) => (
-                        <Record time={time} id={index} key={index} />
-                    ))}
-                </div>
+                <>
+                    <div className="inner-history-box">
+                        {timeList.map((time, index) => (
+                            <Record time={time} id={index} key={index} />
+                        ))}
+                    </div>
+                    <div className="total-box">
+                        <p>Total time used : {convToMilTime(totalMinsUsed)}</p>
+                    </div>
+                </>
             )}
         </div>
     );
