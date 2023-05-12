8cfc748265c96e3387073adfa6fb0599d8c5d1ba
VickerTan <vickerpangestan@gmail.com>
2023-05-13 00:39:44 +0800

    Improve accessibility for resetting data in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,15 @@ const App = () => {
         mins: 20,
     };
 
+    const validHour = String(limit.hour).padStart(2, "0");
+    const validMins = String(limit.mins).padStart(2, "0");
+
     const [timeList, setTimeList] = useState([]);
     const [overLimit, setOverLimit] = useState(false);
-
-    const [hourLimit, setHourLimit] = useState(
-        String(limit.hour).padStart(2, "0")
-    );
-    const [minsLimit, setMinsLimit] = useState(
-        String(limit.mins).padStart(2, "0")
-    );
-    const [hourLeft, setHourLeft] = useState(
-        String(limit.hour).padStart(2, "0")
-    );
-    const [minsLeft, setMinsLeft] = useState(
-        String(limit.mins).padStart(2, "0")
-    );
+    const [hourLimit, setHourLimit] = useState(validHour);
+    const [minsLimit, setMinsLimit] = useState(validMins);
+    const [hourLeft, setHourLeft] = useState(validHour);
+    const [minsLeft, setMinsLeft] = useState(validMins);
 
     const [totalMinsUsed, setTotalMinsUsed] = useState(0);
 
@@ -91,7 +85,16 @@ const App = () => {
                 totalMinsUsed={totalMinsUsed}
                 setTotalMinsUsed={setTotalMinsUsed}
             />
-            <TimeHistory timeList={timeList} totalMinsUsed={totalMinsUsed} />
+            <TimeHistory
+                hourLimit={hourLimit}
+                minsLimit={minsLimit}
+                timeList={timeList}
+                setTimeList={setTimeList}
+                setHourLeft={setHourLeft}
+                setMinsLeft={setMinsLeft}
+                totalMinsUsed={totalMinsUsed}
+                setTotalMinsUsed={setTotalMinsUsed}
+            />
             <div className="note">
                 <p>What did you have for lunch?</p>
                 <input type="text" />
