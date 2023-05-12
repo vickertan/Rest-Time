5143858102e8fe13ead9407b135c001a5a74d1f0
VickerTan <vickerpangestan@gmail.com>
2023-05-13 00:38:36 +0800

    Add reset button
    Rerender component on reset button

--- src/components/TimeHistory.jsx
+++ src/components/TimeHistory.jsx
@@ -1,12 +1,35 @@
 import convToMilTime from "../func/convToMilTime";
 
-export default function TimeHistory({ timeList, totalMinsUsed }) {
+export default function TimeHistory({
+    hourLimit,
+    minsLimit,
+    timeList,
+    setTimeList,
+    setHourLeft,
+    setMinsLeft,
+    totalMinsUsed,
+    setTotalMinsUsed,
+}) {
+    function handleReset() {
+        if (
+            confirm("Deleted data can't be retrieved. Do you want to proceed?")
+        ) {
+            setTimeList([]);
+            setHourLeft(hourLimit);
+            setMinsLeft(minsLimit);
+            setTotalMinsUsed(0);
+        }
+    }
+
     return (
         <div className="time-history">
             {!timeList.length ? (
                 <p className="default">No record yet</p>
             ) : (
                 <>
+                    <button className="reset-button" onClick={handleReset}>
+                        Clear
+                    </button>
                     <div className="inner-history-box">
                         {timeList.map((time, index) => (
                             <Record time={time} id={index} key={index} />
