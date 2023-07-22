1b7c90b0df19b6e514bf2501d6fac5ded0d6bd8c
VickerTan <vickerpangestan@gmail.com>
2023-07-22 21:28:09 +0800

    Create quote component logic
    Fetch data from API ninjas

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
+
 function Quote() {
+    const [category, setCategory] = useState("");
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+
+    // fetch quote's data from API Ninjas
+    useEffect(() => {
+        fetch("https://api.api-ninjas.com/v1/quotes?category=happiness", {
+            method: "GET",
+            headers: {
+                "X-Api-Key": "F9BuIfa9FQbnDnA3nHl+5Q==CodlQToENsszxSAJ",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setCategory(data[0].category);
+                setQuote(data[0].quote);
+                setAuthor(data[0].author);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
     return (
-        <div>
-            <h1>Quote here</h1>
-            <p>Author</p>
-        </div>
+        <>
+            <div>
+                <p>{category}</p>
+                <p>{quote}</p>
+                <p>{author}</p>
+            </div>
+        </>
     );
 }
 
