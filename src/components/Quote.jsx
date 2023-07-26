83bf5e8135b9e38fc5b33765a208e7bfb7a7ff22
VickerTan <vickerpangestan@gmail.com>
2023-07-26 21:58:49 +0800

    Use async await to fetch data from API

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,33 +1,53 @@
 import { useState, useEffect } from "react";
 
 function Quote() {
-    const [category, setCategory] = useState("");
-    const [quote, setQuote] = useState("");
-    const [author, setAuthor] = useState("");
+    const [quoteData, setQuoteData] = useState({
+        category: "",
+        content: "",
+        author: "",
+        timeCreated: "",
+    });
 
-    // fetch quote's data from API Ninjas
+    const fetchQuote = async () => {
+        try {
+            const response = await fetch(
+                "https://api.api-ninjas.com/v1/quotes?category=",
+                {
+                    method: "GET",
+                    headers: {
+                        "X-Api-Key": import.meta.env.VITE_API_KEY,
+                    },
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("quote fetch not ok");
+            }
+
+            const data = await response.json();
+            setQuoteData({
+                category: data[0].category,
+                content: data[0].quote,
+                author: data[0].author,
+                timeCreated: "now",
+            });
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    };
+
     useEffect(() => {
-        fetch("https://api.api-ninjas.com/v1/quotes?category=happiness", {
-            method: "GET",
-            headers: {
-                "X-Api-Key": "F9BuIfa9FQbnDnA3nHl+5Q==CodlQToENsszxSAJ",
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setCategory(data[0].category);
-                setQuote(data[0].quote);
-                setAuthor(data[0].author);
-            })
-            .catch((error) => console.log(error));
+        fetchQuote();
     }, []);
+
+    console.log(quoteData);
 
     return (
         <>
             <div>
-                <p>{category}</p>
-                <p>{quote}</p>
-                <p>{author}</p>
+                <p>{quoteData.category}</p>
+                <p>{quoteData.content}</p>
+                <p>{quoteData.author}</p>
             </div>
         </>
     );
