3574d20bdf83f512fc76b0e4f51bb1835f66e943
VickerTan <vickerpangestan@gmail.com>
2024-01-30 16:37:30 +0800

    Change milTimeLeft in local storage to totalMinsLeft
    Update hour and mins left value to display on totalMinsleft change
    
    Update limit status on totalMinsLeft change

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,11 @@ const App = () => {
     const [totalMinsUsed, setTotalMinsUsed] = useState(0);
 
     const [totalMinsLimit, setTotalMinsLimit] = useState(
-        convToMins(`${limit.hour}:${limit.mins}`)
+        convToMins(`${hourLimit}:${minsLimit}`)
     );
-    const [totalMinsLeft, setTotalMinsLeft] = useState(totalMinsLimit);
-
-    // useEffect(() => {
-    //     setTotalMinsLeft(totalMinsLimit - totalMinsUsed);
-    // }, [hourLeft, minsLeft]);
+    const [totalMinsLeft, setTotalMinsLeft] = useState(
+        convToMins(`${hourLeft}:${minsLeft}`)
+    );
 
     // keeps limitStatus's state in local storage
     useEffect(() => {
@@ -46,21 +44,6 @@ const App = () => {
     useEffect(() => {
         localStorage.setItem("limitStatus", JSON.stringify(limitStatus));
     }, [limitStatus]);
-
-    // keeps time left's state in local storage
-    useEffect(() => {
-        const milTimeLeft_S = JSON.parse(localStorage.getItem("milTimeLeft"));
-        if (milTimeLeft_S) {
-            setHourLeft(milTimeLeft_S.split(":")[0]);
-            setMinsLeft(milTimeLeft_S.split(":")[1]);
-        }
-    }, []);
-    useEffect(() => {
-        localStorage.setItem(
-            "milTimeLeft",
-            JSON.stringify(convToMilTime(totalMinsLeft))
-        );
-    }, [totalMinsLeft]);
 
     // keeps timeList in local storage
     useEffect(() => {
@@ -85,6 +68,36 @@ const App = () => {
     useEffect(() => {
         localStorage.setItem("totalMinsUsed", JSON.stringify(totalMinsUsed));
     }, [totalMinsUsed]);
+
+    // update hour and mins left's state on totalMinsLeft change
+    useEffect(() => {
+        const totalMinsLeft_S = JSON.parse(
+            localStorage.getItem("totalMinsLeft")
+        );
+        if (totalMinsLeft_S) {
+            const milTimeLeft = convToMilTime(totalMinsLeft_S);
+            setTotalMinsLeft(totalMinsLeft_S);
+            setHourLeft(milTimeLeft.split(":")[0]);
+            setMinsLeft(milTimeLeft.split(":")[1]);
+        }
+    }, []);
+    useEffect(() => {
+        localStorage.setItem("totalMinsLeft", totalMinsLeft);
+        const milTimeLeft = convToMilTime(totalMinsLeft);
+        setHourLeft(milTimeLeft.split(":")[0]);
+        setMinsLeft(milTimeLeft.split(":")[1]);
+    }, [totalMinsLeft]);
+
+    // update limitStatus on totalMinsLeft change
+    useEffect(() => {
+        if (totalMinsLeft < 0) {
+            setLimitStatus("danger");
+        } else if (totalMinsLeft < 10) {
+            setLimitStatus("warn");
+        } else {
+            setLimitStatus("");
+        }
+    }, [totalMinsLeft]);
 
     if (limitStatus === "danger") {
         limitColor = "#aa1e3a";
