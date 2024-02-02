d53262f3f69e1982f81cd27057c255087613a4b5
VickerTan <vickerpangestan@gmail.com>
2024-02-02 22:10:47 +0800

    Add update log in info.md file
    Fetch info.md content into a modal

--- src/components/InfoButton.jsx
+++ src/components/InfoButton.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
+import Markdown from "react-markdown";
 
 export default function InfoButton() {
     const [open, setOpen] = useState(false);
+
+    const [content, setContent] = useState();
+
+    useEffect(() => {
+        fetch("./assets/info.md")
+            .then((res) => res.text())
+            .then((text) => setContent(text));
+    }, []);
 
     return (
         <div className="info">
@@ -32,17 +41,22 @@ export default function InfoButton() {
                     <Box
                         sx={{
                             position: "absolute",
+                            height: "50%",
+                            width: "70%",
+                            maxWidth: "400px",
                             top: "50%",
                             left: "50%",
                             transform: "translate(-50%, -50%)",
-                            width: 400,
-                            bgcolor: "background.paper",
+                            bgcolor: "beige",
                             border: "2px solid #000",
                             boxShadow: 24,
                             p: 4,
                             outline: 0,
+                            overflow: "scroll",
                         }}
-                    ></Box>
+                    >
+                        <Markdown>{content}</Markdown>
+                    </Box>
                 </Fade>
             </Modal>
         </div>
