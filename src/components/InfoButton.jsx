965ecbe6797da42157e1a69802a14c3a3c7c2ce7
VickerTan <vickerpangestan@gmail.com>
2024-02-03 06:51:27 +0800

    Change uppercase to lowercase

--- src/components/InfoButton.jsx
+++ src/components/InfoButton.jsx
@@ -12,7 +12,7 @@ export default function InfoButton() {
     const [content, setContent] = useState();
 
     useEffect(() => {
-        fetch("./Public/info.md")
+        fetch("./public/info.md")
             .then((res) => res.text())
             .then((text) => setContent(text));
     }, []);
