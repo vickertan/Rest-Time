0de334515b3b3fc0894399f34d64d461514f745a
VickerTan <vickerpangestan@gmail.com>
2024-10-30 21:54:07 +0800

    Refactoring PWA manifest setup using vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            includeAssets: ["**/*"],
+            manifest: {
+                name: "Break Time",
+                short_name: "Break",
+                icons: [
+                    {
+                        src: "./assets/rest-icon.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                    {
+                        src: "./assets/rest-icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+                theme_color: "#000",
+                background_color: "#000",
+                start_url: "https://rest-time.vercel.app/",
+                display: "standalone",
+                orientation: "portrait",
+            },
+        }),
+    ],
 });
